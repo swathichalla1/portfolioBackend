d82e12b128ed2a2554b6c5138d87f5d3e15f5b06
swathi <swathichalla0@gmail.com>
2024-02-01 16:39:14 +0530

    first commit

---
+++ db/connection.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+//  mongoose.connect('mongodb://127.0.0.1/PortfolioContact') -- local connection using mongodb compass
+mongoose.connect('mongodb+srv://swathi:mongodb123@cluster0.qzileql.mongodb.net/PortfolioData?retryWrites=true&w=majority')
